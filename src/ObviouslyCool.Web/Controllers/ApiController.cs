2af4243fe0a0ab7ce3d269ca2b5a9a39f0ae39d7
Adam Swanson <adam.swanson.98@gmail.com>
2022-08-29 18:41:41 -0400

    clean: remove default WeatherForecastController

---
+++ src/ObviouslyCool.Web/Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ObviouslyCool.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("API was called.");
+            return "It works!";
+        }
+    }
+}
