084e9ee6ed0846d03869f493245f55d2be94d9ee
Adam Swanson <adam.swanson.98@gmail.com>
2022-09-06 22:04:10 -0400

    feat: update socials

--- src/ObviouslyCool.Web/ClientApp/src/app/home/home.component.ts
+++ src/ObviouslyCool.Web/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ExternalNavigationItem, InternalNavigationItem, NavigationItem } from '../navigation/models';
-import { faTwitter, faYoutube, faTwitch, faGithub, faSteam, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faYoutube, faTwitch, faGithub, faSteam, IconDefinition, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 export class Social {
   icon: IconDefinition;
@@ -30,13 +30,15 @@ export class HomeComponent {
   faTwitch = faTwitch;
   faGithub = faGithub;
   faSteam = faSteam;
+  faDiscord = faDiscord;
 
   socials: Social[] = [
+    new Social(this.faTwitch, 'https://twitch.tv/crisp2020'),
+    new Social(this.faDiscord, 'https://discord.gg/2DFqfgVVYm'),
     new Social(this.faTwitter, 'https://twitter.com/crisp2020'),
     new Social(this.faYoutube, 'https://youtube.com/crisp2020'),
-    new Social(this.faTwitch, 'https://twitch.tv/crisp2020'),
-    new Social(this.faGithub, 'https://github.com/theaswanson'),
     new Social(this.faSteam, 'https://steamcommunity.com/id/crisp2020'),
+    new Social(this.faGithub, 'https://github.com/theaswanson'),
   ];
 
   constructor() { }
